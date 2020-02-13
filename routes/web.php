167311177ae1e20a24679477b32401f347cd9256
yejiepeng <qq757807960@gmail.com>
2020-02-13 14:19:31 +0800

    fix bug

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,10 @@ Route::get('tradingview', 'RegisterController@tradingview');
 
 Route::get('/download', 'DownloadController@index');
 
+// 转盘抽奖
+Route::group(['prefix' => 'lottery'], function () {
+    Route::get('index', 'LotteryController@index'); // 抽奖页面
+    Route::get('log', 'LotteryController@log'); // 抽奖记录
+    Route::get('info', 'LotteryController@info'); // 抽奖说明
+    Route::post('submit', 'LotteryController@submit'); // 抽奖提交
+});
