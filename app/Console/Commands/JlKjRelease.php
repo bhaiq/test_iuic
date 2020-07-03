807b9b9b3484a585e7d4faf6927c95b679bc9b1a
bhai <2283814227@qq.com>
2020-07-03 16:07:49 +0800

    矿机算力释放

--- app/Console/Commands/JlKjRelease.php
+++ app/Console/Commands/JlKjRelease.php
@@ -44,10 +44,11 @@ class JlKjRelease extends Command
     {
         //获取所有用户开通矿位信息
         Log::info("矿机算力开始释放");
-        $kuangji_list = KuangjiUserPosition::where('order_id','>',1)->where('kuangji_id','>',0)->get();
+        $kuangji_list = KuangjiUserPosition::where('order_id','>',0)->where('kuangji_id','>',0)->get();
         foreach ($kuangji_list as $k => $kuangji){
             //当前矿位矿机算力
             $suanli = Kuangji::where('id',$kuangji->kuagnji_id)->value('suanli');
+            Log::info("算力",['suanli'=>$suanli]);
             //user_info中增加release_total
             // 释放矿池数增加,不能大于user_info->buy_total
             $user_info = UserInfo::where('uid',$kuangji->uid)->first();
