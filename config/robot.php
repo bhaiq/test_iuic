aa99c705cbfe0296d2e167f5f3a4893d3e894861
bhai <2283814227@qq.com>
2020-07-11 17:48:20 +0800

    机器人设置

---
+++ config/robot.php
@@ -0,0 +1,12 @@
+<?php return array (
+    'robot_switch' => '1',
+    'robot_start_time' => '00:00:00',
+    'robot_end_time' => '23:59:59',
+    'robot_min_range' => '0.0001',
+    'robot_max_range' => '0.001',
+    'robot_trade_status' => '1',
+    'robot_max_price' => '0.28',
+    'robot_min_price' => '0.20',
+    'robot_min_trade_num' => '100',
+    'robot_max_trade_num' => '500',
+);
