0b0eb6baf24b3f6952b28d5d08e8e67f8da306f8
bhai <2283814227@qq.com>
2020-09-20 10:42:39 +0800

    关闭能量释放

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -760,7 +760,7 @@ class ExcelController extends Controller
                     //能量订单处理
                     EnergyOrder::where('uid',$user['id'])->update(['status'=>"1"]);
 //                  // 补上IUIC矿池
-                    UserInfo::where('uid', $user['id'])->increment(['buy_total'=>$v[1]]);
+                    UserInfo::where('uid', $user['id'])->increment('buy_total',$v[1]);
 //                    // IUIC灵活矿机 清空
 //                    KuangjiLinghuo::where('uid', $user['id'])->update(['num'=>'0']);
 
