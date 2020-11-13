8595b059bd7e1caff41bfe78071635638d35c363
bhai <2283814227@qq.com>
2020-11-13 14:31:58 +0800

    测试月度分红

--- app/Console/Commands/CommDividMonth.php
+++ app/Console/Commands/CommDividMonth.php
@@ -32,7 +32,8 @@ class CommDividMonth extends Command {
               	
               	$total_yj_one=CommunityDividend::where('uid',$list[$j]['uid'])->value('last_month');
                     
-              	if($total_yj_one<=0){                 
+              	if($total_yj_one<=0){
+                    \Log::info('业绩小于0跳过');
                 	continue;
               	}	
                     
@@ -55,6 +56,7 @@ class CommDividMonth extends Command {
                 for($i=0;$i<count($teams);$i++){
           
                     if($yf_bl>=$all_star_bl){
+                        \Log::info("已返比例大于总比例");
                          break;
                     }
 
@@ -63,13 +65,13 @@ class CommDividMonth extends Command {
                     
                     //判断该比例是否返过
                     if(in_array($bl,$data_yfbl)){
-                        Log::info("该比例是返过");
+                        \Log::info("该比例是返过");
                          continue;
                     }
 					//如果已经返过0.2则不返0.1
 					if(count($data_yfbl)>0){
 						if(max($data_yfbl)>$bl){
-                            Log::info("已经返过0.2则不返0.1");
+                            \Log::info("已经返过0.2则不返0.1");
 							continue;
 						}
 					}
@@ -77,7 +79,7 @@ class CommDividMonth extends Command {
 					$total_yj=CommunityDividend::where('uid',$teams[$i])->value('last_month');
 				
 					if($total_yj<=0){
-                        Log::info("total_yj小于0");
+                        \Log::info("total_yj小于0");
 						 continue;
 					}	
                     
