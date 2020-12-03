14dbe19e0d98e106ce969f9c913b1809390260f5
bhai <2283814227@qq.com>
2020-12-03 21:11:02 +0800

    用户关系恢复

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -872,7 +872,7 @@ class ExcelController extends Controller
     {
         \Log::info('用户处理资产开始');
 
-        $data = Excel::toArray(new UsersImport,storage_path('/exports/user.xls'));
+        $data = Excel::toArray(new UsersImport,storage_path('/exports/user.xlsx'));
         $count = count($data);
         if($count < 1){
             return returnJson('0','未检测到有效数据');
