338f5f5f7884278058c30431d1c0d2c618303ef9
bhai <2283814227@qq.com>
2021-01-03 16:40:45 +0800

    加速释放测试

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,7 +60,7 @@ class User extends Model
 {
     protected $table = 'user';
 
-    protected $fillable = ['mobile', 'email', 'password', 'pid', 'invite_code', 'type', 'path', 'pid_path', 'nickname', 'new_account','int_code'];
+    protected $fillable = ['mobile', 'email', 'password', 'pid', 'invite_code', 'type', 'path', 'pid_path', 'nickname', 'new_account','int_code','created_at'];
 
     protected $hidden = ['password', 'transaction_password'];
 
