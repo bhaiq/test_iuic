71892c03a76406fbaf453f6635f1d9b7cec29c8c
bhai <2283814227@qq.com>
2021-01-14 20:39:49 +0800

    划转加请求限制

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -348,7 +348,7 @@ class AccountController extends Controller
                     ->orderBy('id','desc')
                     ->first();
 //                    ->value('created_at');
-        dd(strtotime($last_log->created_at));
+        dd(strtotime($last_log));
         if($times - strtotime($last_log->created_at) <= 5){
             return $this->responseError("请求频繁");
         }
